c38481e0ece8dd755c4d70dc67c4a3e400be0192
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-06-01 18:04:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 from flask import Flask, request
-import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def show_ip():
     user_ipv4 = request.headers.get('X-Forwarded-For').split(',')[0].strip()
-    try:
-        user_ipv6 = socket.gethostbyname(user_ipv4)
-    except:
-        user_ipv6 = 0
-
-    return f"Your IPv4 address is: {user_ipv4}<br>Your IPv6 address is: {user_ipv6}"
+    return f"Your IPv4 address is: {user_ipv4}"
 
 if __name__ == '__main__':
     app.run()
