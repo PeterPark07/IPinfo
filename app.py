88c96cafc791e491cc6c8e4a11daddbb0fa3400a
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-06-01 17:42:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def show_ip():
     user_ipv4 = request.headers.get('X-Forwarded-For').split(',')[0].strip()
-    try :
-        user_ipv6 = request.headers.get(socket.getaddrinfo(user_ipv4, None, socket.AF_INET6))
+    try:
+        addr_info = socket.getaddrinfo(user_ipv4, None, socket.AF_INET6)
+        user_ipv6 = addr_info[0][4][0] if addr_info else None
     except:
-        user_ipv6 = 0000
+        user_ipv6 = 0
 
     return f"Your IPv4 address is: {user_ipv4}<br>Your IPv6 address is: {user_ipv6}"
 
