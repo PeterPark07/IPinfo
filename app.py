47c623f2206ad002529f7d869e06acaf3512ebd6
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-06-01 17:59:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,7 @@ app = Flask(__name__)
 def show_ip():
     user_ipv4 = request.headers.get('X-Forwarded-For').split(',')[0].strip()
     try:
-        addr_info = socket.getaddrinfo(user_ipv4, None, socket.AF_INET6)
-        for addr in addr_info:
-            if addr[0] == socket.AF_INET6:
-                user_ipv6 = addr[4][0]
-                break
-        else:
-            user_ipv6 = None
+        user_ipv6 = socket.gethostbyname(user_ipv4)
     except:
         user_ipv6 = 0
 
