3bbbd7d8c63d7378933f9b47208155765ec11fc7
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-01-17 00:29:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def show_ip():
     user_ipv4 = request.headers.get('X-Forwarded-For').split(',')[0].strip()
+    print(request.headers.get('X-Forwarded-For'))
     return f"Your IPv4 address is: {user_ipv4}"
-
-@app.route('/ip')
-def get_user_ip():
-    user_ip = request.remote_addr
-    return f'Your IP address is: {user_ip}'
 
 if __name__ == '__main__':
     app.run()
