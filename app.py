3f2b05a3fecfbf35cc9954d2b7ded0cd98c6b346
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-01-17 00:15:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ def show_ip():
     user_ipv4 = request.headers.get('X-Forwarded-For').split(',')[0].strip()
     return f"Your IPv4 address is: {user_ipv4}"
 
+@app.route('/ip')
+def get_user_ip():
+    user_ip = request.remote_addr
+    return f'Your IP address is: {user_ip}'
+
 if __name__ == '__main__':
     app.run()
